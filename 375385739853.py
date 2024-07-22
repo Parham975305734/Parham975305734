c46dd6a0a8680331c25f701d759aebdeb2965a3e
Parham975305734 <parhamsadeghifar8@gmail.com>
2024-07-22 22:59:39 +0330

    Add files via upload

---
+++ 375385739853.py
@@ -0,0 +1,10 @@
+input:3
+print("Good midnight")
+input:8
+print("Good morning")
+input:15
+print('Good afternoon')
+input:8
+print("Good morning")
+input:25
+print("Sara khanom Lotfan Adadi Dar Baze 0 ta 24 vared kon")
